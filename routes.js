d746058e19d6c9f0be704eea28e49e9419a8677b
Stella Dunn <stellacdunn@gmail.com>
2023-11-06 18:11:41 -0700

    Renamed routes

--- routes.js
+++ routes.js
@@ -1,8 +1,8 @@
 
-import { router as userRouter } from './routes/user.js';
-import { router as reasonRouter } from './routes/reason.js';
-import { router as evictionRouter } from './routes/eviction.js';
-import { router as searchRouter } from './routes/search.js'
-import { router as testClientRouter } from './routes/testClient.js';
+import { router as userRouter } from './routes/userRoutes.js';
+import { router as reasonRouter } from './routes/reasonRoutes.js';
+import { router as evictionRouter } from './routes/evictionRoutes.js';
+import { router as searchRouter } from './routes/searchRoutes.js'
+import { router as testClientRouter } from './routes/testClientRoutes.js';
 
 export { evictionRouter, reasonRouter, searchRouter, testClientRouter, userRouter }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { User, getUserById, updateUser } from '../models/User.js';
+import bcrypt from 'bcrypt';
 
 var router = Router();
 
@@ -21,31 +22,41 @@ router.get('/:id', async (req, res) => {
 // TODO: needs to check :auth
 router.post('/', async (req, res) => {
 	try {
+		let checkExists = await findUser(req.body.username);
+
+		if (checkExists) {
+			res.status(401).json({ message: "Email is already in use." });
+			return;
+		}
+		const saltRounds = 10;
+		const passHash = bcrypt.hashSync(req.body.password, saltRounds);
 		const user = new User({
 			username: req.body.username,
-			passHash: req.body.passHash,
+			passHash: passHash,
 			facilityName: req.body.facilityName,
 			facilityAddress: req.body.facilityAddress,
 			facilityPhone: req.body.facilityPhone,
 			facilityEmail: req.body.facilityEmail
 		});
-		await user.save();
-		res.send(user);
-	} catch {
-		res.status(500);
-		res.send({ error: 'Internal Server Error' });
+		user.save().then(() => {
+			res.json({ message: "User created successfully", user });
+		});
+	} catch (err) {
+		res.status(401);
+		res.send(err.message);
 	}
-})
+});
 
 /* PATCH - update user (user- and admin-only) */
 // TODO: needs to check :auth
 router.patch('/:id', async (req, res) => {
 	try {
-		const userID = req.params.id
+		const userID = req.params.id;
 		// var user = await getUserById(userID);
-		var updateParams = {}
+		var updateParams = {};
+		const saltRounds = 10;
 		//username: req.body.username ? req.boy.username : {}, // Don't allow users to change own username
-		if (req.body.passHash) { updateParams.passHash = req.body.passHash };
+		if (req.body.passHash) { updateParams.passHash = bcrypt.hashSync(req.body.password, saltRounds) };
 		if (req.body.facilityName) { updateParams.facilityName = req.body.facilityName };
 		if (req.body.facilityAddress) { updateParams.facilityAddress = req.body.facilityAddress };
 		if (req.body.facilityPhone) { updateParams.facilityPhone = req.body.facilityPhone };
