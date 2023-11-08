0ca63fa92bae1f62edb0f65e19bfd1cfc6fc0a78
Stella Dunn <stellacdunn@gmail.com>
2023-11-07 17:13:56 -0700

    Added JWT and authentication middleware

--- routes/testClientRoutes.js
+++ routes/testClientRoutes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { authenticateToken } from '../middleware/authenticateToken';
+import { authenticateToken } from '../middleware/authenticateToken.js';
 var router = Router();
 
 /* GET home page. */
