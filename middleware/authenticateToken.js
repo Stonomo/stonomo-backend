afa046fe7e52126033b2cba0af35608a7c4d6b4b
Stella Dunn <stellacdunn@gmail.com>
2024-02-06 07:40:31 -0700

    STON-3 Moved TOKEN_SECRET to secrets file

--- middleware/authenticateToken.js
+++ middleware/authenticateToken.js
@@ -1,19 +1,28 @@
 import jwt from 'jsonwebtoken'
+import { readFile } from 'fs/promises'
 
-export function authenticateToken(req, res, next) {
+export async function authenticateToken(req, res, next) {
 	const authHeader = req.headers['authorization']
 	const token = authHeader && authHeader.split(' ')[1]
 
 	if (token == null) return res.sendStatus(401)
 
-	jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
-		if (err) {
-			console.log(err)
-			return res.sendStatus(403)
+	const TOKEN_SECRET = await readFile(
+		process.env.TOKEN_SECRET_FILE
+	)
+
+	jwt.verify(
+		token,
+		TOKEN_SECRET,
+		(err, user) => {
+			if (err) {
+				console.log(err)
+				return res.sendStatus(403)
+			}
+
+			req.user = user
+
+			next()
 		}
-
-		req.user = user
-
-		next()
-	})
+	)
 }
