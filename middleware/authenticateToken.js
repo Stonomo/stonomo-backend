4b8251c3759a47e3940c10bec81fe9eef0b17f17
Stella Dunn <stellacdunn@gmail.com>
2024-02-09 10:07:09 -0700

    Refactored JWT logic for better reuse

--- middleware/authenticateToken.js
+++ middleware/authenticateToken.js
@@ -1,5 +1,6 @@
 import jwt from 'jsonwebtoken'
 import { readFile } from 'fs/promises'
+import { getTokenSecret } from '../lib/jwtHelper.js'
 
 export async function authenticateToken(req, res, next) {
 	const authHeader = req.headers['authorization']
@@ -7,13 +8,9 @@ export async function authenticateToken(req, res, next) {
 
 	if (token == null) return res.sendStatus(401)
 
-	const TOKEN_SECRET = await readFile(
-		process.env.TOKEN_SECRET_FILE
-	)
-
 	jwt.verify(
 		token,
-		TOKEN_SECRET,
+		getTokenSecret(),
 		(err, user) => {
 			if (err) {
 				console.log(err)
