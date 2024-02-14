ff252592e544daf3ea728b0b0833538f790fa803
Stella Dunn <stellacdunn@gmail.com>
2024-02-14 07:47:34 -0700

    Removed commented-out code

--- middleware/authenticateToken.js
+++ middleware/authenticateToken.js
@@ -2,8 +2,6 @@ import jwt from 'jsonwebtoken'
 import { getTokenSecret } from '../lib/jwtHelper.js'
 
 export async function authenticateToken(req, res, next) {
-	// const authHeader = req.headers['authorization']
-	// const token = authHeader && authHeader.split(' ')[1]
 	const token = req.signedCookies?.stonomoToken;
 
 	if (token == null) { console.log("null token"); return res.sendStatus(401); }
