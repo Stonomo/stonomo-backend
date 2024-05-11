2b163de61f5484ba8a558829baaa68b80a225c87
Stella Dunn <stellacdunn@gmail.com>
2024-05-11 09:34:43 -0600

    Added route to load test evictions. Added placeholder admin auth check.

---
+++ middleware/authenticateAdminToken.js
@@ -0,0 +1,24 @@
+import jwt from 'jsonwebtoken'
+import { getTokenSecret } from '../lib/jwtHelper.js'
+
+export async function authenticateAdminToken(req, res, next) {
+	const token = req.signedCookies?.stonomoToken;
+
+	if (token == null) { console.log("null token"); return res.sendStatus(401); }
+
+	jwt.verify(
+		token,
+		getTokenSecret(),
+		(err, user) => {
+			if (err) {
+				console.log(err)
+				return res.sendStatus(403);
+			}
+
+			req.user = user;
+
+			//TODO: add admin check
+			next();
+		}
+	)
+}
