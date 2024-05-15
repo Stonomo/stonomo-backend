636a65085268a5f3b71dafe903b1f49cf3029f54
Stella Dunn <stellacdunn@gmail.com>
2024-05-14 20:00:03 -0600

    Added refresh token rotation with replay attack protection

--- routes/rootRoutes.js
+++ routes/rootRoutes.js
@@ -1,8 +1,15 @@
 import { Router } from 'express';
+import jwt from 'jsonwebtoken'
 import bcrypt from 'bcrypt';
 
 import { getUserByUsername } from '../models/User.js';
-import { generateAccessToken, generateRefreshToken } from '../lib/jwtHelper.js';
+import {
+	generateAccessToken,
+	generateRefreshToken,
+	getRefreshTokenSecret,
+	generateNonce
+} from '../lib/jwtHelper.js';
+import { Token } from '../models/Token.js';
 
 var router = Router();
 
@@ -33,5 +40,62 @@ router.post('/login', async (req, res) => {
 		res.status(401).send(err.message);
 	}
 });
+
+// Refresh token route
+router.post('/refresh-token', async (req, res) => {
+	const { refreshToken } = req.body;
+	let createNewTokenFamily = false
+
+	if (!refreshToken) {
+		return res.status(403).json({ message: 'Refresh token is required' });
+	}
+
+	jwt.verify(refreshToken, getRefreshTokenSecret(), async (err, decoded) => {
+		if (err) {
+			return res.status(403).json({ message: 'Invalid refresh token' });
+		}
+
+		const { name, tokenType, tokenFamily, nonce } = decoded;
+
+		if (tokenType !== 'refresh') {
+			return res.status(403).json({ message: 'Invalid token type' });
+		}
+
+		// Check if nonce is valid
+		const existingToken = await Token.findOne({ tokenFamily, tokenType: 'refresh', nonce });
+
+		if (!existingToken) {
+			// Invalidate all tokens of the same family
+			await Token.deleteMany({ tokenFamily });
+			return res.status(403).json({ message: 'Invalid nonce. Token family invalidated.' });
+			createNewTokenFamily = true;
+		}
+
+		// Check if the existing token is the most recent one
+		const newerTokenExists = await Token.exists({ tokenFamily, tokenType: 'refresh', createdAt: { $gt: existingToken.createdAt } });
+
+		if (newerTokenExists) {
+			// Invalidate all tokens of the same family
+			await Token.deleteMany({ tokenFamily });
+			return res.status(403).json({ message: 'Existing token is not the most recent one. Token family invalidated.' });
+			createNewTokenFamily = true;
+		}
+
+		let newTokenFamily = tokenFamily;
+		if (createNewTokenFamily) {
+			newTokenFamily = generateNonce();
+		}
+
+		const user = await getUserByUsername(name);
+
+		// Generate new access and refresh tokens with a new nonce
+		const newNonce = generateNonce();
+		const newAccessToken = await generateAccessToken(user, newTokenFamily, newNonce);
+		const newRefreshToken = await generateRefreshToken(user, newTokenFamily, newNonce);
+
+		res.json({ accessToken: newAccessToken, refreshToken: newRefreshToken });
+	});
+});
+
 
 export { router };
