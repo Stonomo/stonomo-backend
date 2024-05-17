2b99418895f3925225d57f46ffbcbf28c92a9d83
Stella Dunn <stellacdunn@gmail.com>
2024-05-16 18:40:03 -0600

    Added missing token checks

--- middleware/authenticateToken.js
+++ middleware/authenticateToken.js
@@ -1,5 +1,6 @@
 import jwt from 'jsonwebtoken'
 import { getTokenSecret } from '../lib/jwtHelper.js'
+import { Token } from '../models/Token.js';
 
 export async function authenticateToken(req, res, next) {
 	try {
@@ -13,14 +14,30 @@ export async function authenticateToken(req, res, next) {
 		jwt.verify(
 			accessToken,
 			getTokenSecret(),
-			(err, user) => {
+			async (err, payload) => {
 				if (err) {
 					console.error('Invalid token')
 					console.error(err)
 					return res.status(403).send('Invalid Token');
 				}
 
-				req.user = user;
+				const existingToken = Token.find({ tokenFamily: payload.tokenFamily, tokenType: 'access', nonce: payload.nonce });
+
+				if (!existingToken) {
+					// Invalidate all tokens of the same family
+					await Token.deleteMany({ tokenFamily: payload.tokenFamily });
+					return res.status(403).json({ message: 'Invalid nonce. Token family invalidated.' });
+				}
+
+				const newerTokenExists = await Token.exists({ tokenFamily: payload.tokenFamily, tokenType: 'access', createdAt: { $gt: existingToken.createdAt } });
+
+				if (newerTokenExists) {
+					// Invalidate all tokens of the same family
+					await Token.deleteMany({ tokenFamily: payload.tokenFamily });
+					return res.status(403).json({ message: 'Existing token is not the most recent one. Token family invalidated.' });
+				}
+
+				req.access = payload;
 
 				next();
 			}
