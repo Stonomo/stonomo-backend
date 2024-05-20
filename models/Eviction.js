e8956d322f493822745ffaca1666ac88b9ff81e5
Stella Dunn <stellacdunn@gmail.com>
2024-05-20 14:40:01 -0600

    Fixed bug where nameMatches never showed true

--- models/Eviction.js
+++ models/Eviction.js
@@ -183,7 +183,7 @@ export async function searchForEviction(searchName, searchPhone, searchEmail) {
 					'$cond': {
 						'if': {
 							'$eq': [
-								'$tenantName', searchNameLower
+								'$tenantNameLower', searchNameLower
 							]
 						},
 						'then': 1,
